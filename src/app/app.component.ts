1c7ab45556b7ec21f56db56e12d0a58ff595cc53
Esteban <davidemg903@gmail.com>
2024-09-20 16:43:10 -0500

    formulario actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SegundoComponenteComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'formulario';
+  mensaje = "hola a todos";
+
 }
